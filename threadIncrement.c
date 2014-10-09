3abc69e33c1108ee7fc003399c98d74719f21ffa
garyleejohnsonjr <garyjohnson@my.unt.edu>
2014-10-09 12:30:58 -0500

    Update threadIncrement.c

--- threadIncrement.c
+++ threadIncrement.c
@@ -1,21 +1,24 @@
+#include <pthread.h>
 #include <stdio.h>
 
-void inc_n(int *n)
+void* inc_n(void* n)
 {
    /* increment n to 100 */
-   while(++(*n) < 1000000000);
+   while(++((int*)n) < 1000000000);
+   pthread_exit(NULL);
 }
 
 int main()
 {
+    pthread_t tid[2];
 
     int x = 0, y = 0;
     printf("x: %d, y: %d\n", x, y);
-   
-    inc_n(&x);
+    pthread_create(&tid[0], NULL, inc_n, &x);
+    pthread_create(&tid[1], NULL, inc_n, &y);
+    pthread_join(tid[0], NULL);
     printf("x increment finished\n");
-   
-    inc_n(&y);
+    pthread_join(tid[1], NULL);
     printf("y increment finished\n");
     printf("x: %d, y: %d\n", x, y);
     return 1;
